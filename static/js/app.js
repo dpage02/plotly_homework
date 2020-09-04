0c4720404485699b4e6fd690c2f0212110e97cbd
dpage02 <61803281+dpage02@users.noreply.github.com>
2020-09-03 22:46:53 -0400

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,125 @@
+// unpack function that will return information in object 
+function unpack(rows, index) {
+    return rows.map(function (row) {
+        return row[index];
+    });
+};
+
+// load in csv 
+d3.json("samples.json").then(data => {
+    var ids = unpack(data.metadata, "id");
+    var dropdown = d3.select("#selDataset");
+    // append dropdown with ids 
+    dropdown.selectAll("select").data(ids).enter().append("option")
+        .html(function (x) {
+            return `<option value= "${x}">${x}</option>`;
+        });
+});
+
+function init() {
+    d3.json("samples.json").then(data => {
+        var val = data.metadata[0].id.toString();
+        buildPlot(val)
+        console.log(`init ${val}`);
+        buildInfo(val)
+    });
+};
+
+function optionChanged() {
+    var inputEl = d3.select("select");
+    var val = inputEl.property("value");
+    console.log(val);
+    buildPlot(val);
+    buildInfo(val);
+};
+
+function buildPlot(val) {
+    // making sure data is loaded 
+    d3.json("samples.json").then(data => {
+        // console.log(data);
+        // console.log(val)
+
+        /*MAKING HORIZONTAL CHART */
+
+        // getting correct sample 
+        var sample = data.samples.find(({ id }) => id === val);
+        // console.log(sample);
+
+        // getting sample values and reversing order 
+        var sample_values = sample.sample_values;
+        var topSamps = sample_values.slice(0, 10).reverse();
+
+        // getting sample OTU id's
+        var otu_ids = sample.otu_ids;
+        var top_Otu = (otu_ids.slice(0, 10)).reverse();
+
+        // getting OTU labels 
+        var labels = sample.otu_labels;
+        var otu_labels = labels.slice(0, 10).reverse();
+
+        // creating trace 
+        var trace = {
+            x: topSamps,
+            y: top_Otu.map(x => `OTU ${x}  `),
+            text: otu_labels,
+            type: "bar",
+            orientation: "h"
+        }
+
+        // making trace an array 
+        var data = [trace];
+
+        // creating layout 
+        var layout = {
+            title: "",
+            margin: {
+                l: 100,
+                r: 10,
+                t: 20,
+                b: 40
+            }
+        };
+
+        // plotting horizontal bar chart 
+        Plotly.newPlot("bar", data, layout);
+
+        /*MAKING BUBBLE CHART */
+
+        var bubble_trace = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                size: sample_values
+            },
+        }
+
+        var bubble_data = [bubble_trace];
+        var bubble_layout = {
+            height: 800,
+            width: 900
+        };
+
+        Plotly.newPlot("bubble", bubble_data, bubble_layout);
+    });
+};
+
+function buildInfo(val) {
+    d3.json("samples.json").then(data => {
+        var sample = data.metadata.find(({ id }) => id.toString() === val);
+        console.log(sample);
+
+        var demoInfo = d3.select(".panel-body");
+        demoInfo.text("");
+
+        Object.entries(sample).forEach(([k,v])=> {
+            demoInfo.append("li").text(`${k}: ${v} \n`)
+        });
+        
+    });
+};
+
+
+init();
